2075c2d1e204911f4447d1eda4997ab5153e6a5c
ksu.lolenko@gmail.com <ksu.lolenko@gmail.com>
2023-05-05 12:21:26 +0300

    all

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
-import readlineSync from "readline-sync"; 
-console.log('Welcome to the Brain Games!'); 
-export const userName = readlineSync.question("May I have your name? "); 
-console.log('Hello,' + userName + '!' );
+
+import greet from '../src/cli.js';
+
+greet();
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('Your answer: ');
+const greet = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  return userName;
+};
+
+export default (greet);
